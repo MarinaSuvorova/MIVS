cbcab6c6dae7db6a06936baabb946daf16122024
Marina <32.marina.suvorova@gmail.com>
2018-04-08 18:05:19 +0300

    new commit

--- src/main/java/AppMIVS.java
+++ src/main/java/AppMIVS.java
@@ -1,32 +1,8 @@
 
 
 public class AppMIVS {
-
     public static void main(String[] args) throws Exception {
-//        DataStorage dataStorage = new DataStorage();
-//        DataWriter dataWriter = new DataWriter();
-//        dataStorage.storeLoginInfo();
-//        dataStorage.storeUserProperties();
-//        dataStorage.storeCoursesInfo();
-//        dataStorage.storeStudentCourses();
-//        dataStorage.printUsersTable();
-//        dataStorage.setLastID();
-//
-//        System.out.println("\nLast ID = "+dataStorage.getLastID());
-//
-//        dataStorage.storeCoursesInfo();
-//        dataStorage.setLastCourseCode();
-//        dataStorage.printCoursesTable();
-//        System.out.println("\nLast Course Code = "+dataStorage.getLastCourseCode());
         Menu menu = new Menu();
         menu.runWelcomeMenu();
-//int allowedCredits = 12-dataStorage.countStudentsCredits("STU-1005");
-//        System.out.println(dataStorage.countAllowedCourses("STU-1005",allowedCredits));
-//        dataStorage.printStudentsCoursesTable("STU-1005");
-//        dataStorage.deleteUser("LEC-1001");
-//      dataWriter.updateCoursesInfo();
-//      dataWriter.updateStudentCourses();
-//      dataWriter.updateLoginInfo();
-//      dataWriter.updateUserProperties();
     }
 }
